093d0549eb1c751edd19d3e9c2bf009779fbf4ca
jcaiaffa <jonathan.caiaffa@akendos.com>
2016-09-20 10:20:14 -0500

    Slideshow fix to apartment suites pages

--- apartment-suites/two-bedroom-suite/index.php
+++ apartment-suites/two-bedroom-suite/index.php
@@ -92,6 +92,23 @@
     </section>
 
     <?php include('../../template-parts/footer.php'); ?>
+    <script type="text/javascript" src="../../js/jquery.cycle2.min.js"></script>
+    <script type="text/javascript" src="../../js/jquery.cycle2.carousel.js"></script>
+    <script type="text/javascript">
+    jQuery(document).ready(function($){
+        
+        var slideshows = $('.cycle-slideshow').on('cycle-next cycle-prev', function(e, opts) {
+            slideshows.not(this).cycle('goto', opts.currSlide);
+        });
+
+        $('#cycle2 .cycle-slide').click(function(){
+            var index = $('#cycle2').data('cycle.API').getSlideIndex(this);
+            slideshows.cycle('goto', index);
+        });
+
+    });
+
+    </script>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
