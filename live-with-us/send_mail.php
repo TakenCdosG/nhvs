2e7179108585a9949c03751116e9415b584f2d7d
Esteban Rocha <estebanrocha.j@gmail.com>
2017-03-15 15:02:59 -0500

    Adjustments

--- live-with-us/send_mail.php
+++ live-with-us/send_mail.php
@@ -8,13 +8,10 @@ $email = isset($_POST['email'])?$_POST['email']:'';
 $name = isset($_POST['name'])?$_POST['name']:'';
 $checkin = isset($_POST['checkin'])?$_POST['checkin']:'';
 $checkout = isset($_POST['checkout'])?$_POST['checkout']:'';
-$num_room = isset($_POST['num_room'])?$_POST['num_room']:'';
 $room_type = isset($_POST['room_type'])?$_POST['room_type']:'';
 $package = ""; //$package = $_POST['package'];
-$notes = isset($_POST['notes'])?$_POST['notes']:'';
 
 // Optional Fields
-$organization = isset($_POST['organization'])?$_POST['organization']:'';
 $notes = isset($_POST['notes'])?$_POST['notes']:'';
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -38,7 +35,7 @@ if(!$is_root_from && !empty($email) && !empty($name) && !empty($checkin) && !emp
 	// Subject
 	$correo->Subject = $_POST['name']." Has contacted us - Live With Us";
 	// Email_Body
-	$correo->MsgHTML("<h3>".$name."</h3> <p><b>Phone:</b> ".$_POST['phone']."</p><p> <b>Organization:</b> ".$organization."</p> <p> <b>Check In:</b> ".$checkin."</p> <p><b>Check Out:</b> ".$checkout."</p> <p><b>Room Number:</b> ".$num_room."</p> <p><b>Room Type:</b> ".$room_type."</p> <p><b>Notes:</b> ".$notes."</p>");
+	$correo->MsgHTML("<h3>".$_POST['name']."</h3> <p><b>Phone:</b> ".$_POST['phone']."</p><p> <b>Check In:</b> ".$checkin."</p> <p><b>Check Out:</b> ".$checkout."</p> <p><b>Room Type:</b> ".$room_type."</p> <p><b>Package:</b> ".$package."</p> <p><b>Notes:</b> ".$notes."</p>");
 	//Actions
 	if(!$correo->Send()) {
 	  echo "Oops. Something went wrong. Please try again later. " . $correo->ErrorInfo;
